4c95de4545e717af31d09d4a3caaf67701f4ced0
Luan Santos Gonçalves <luansantos95@hotmail.com>
2022-07-31 14:49:31 -0300

    operacoes basicas (create, read, update, delete)

---
+++ src/database/user.dao.js
@@ -0,0 +1,27 @@
+const users = require('./index').db('store').collection('users');
+const UserID = require('mongodb').UserID;
+
+const save = async (nome, email, idade) => {
+    const result = await users.insertOne({nome, email, idade});
+    return result.ops[0];
+};
+
+const getAll = async () => {
+    const cursor = await users.find();
+    return cursor.toArray();
+};
+
+const getById = async (id) => {
+    return await users.findOne({_id:UserID(id)});
+}
+
+const update = async (id, {nome, email, idade}) => {
+    const result = await users.replaceOne({_id:UserID(id)}, {nome, email, idade});
+    return result.ops[0];
+}
+
+const removeById = async id => {
+    await users.deleteOne({_id:UserID(id)});
+}
+
+module.exports = {getAll, getById, removeById, save, update};
