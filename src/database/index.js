16d43da0347a140d175c6b7c03a6bc42cc2a0d6c
Luan Santos Gonçalves <luansantos95@hotmail.com>
2022-07-31 13:14:09 -0300

    configuracao Koa e MongoDB

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+const {MongoClient} = require("mongodb");
+const client = new MongoClient('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+client.connect(err => {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+    console.log("SUCCESSFULLY connected to MongoDB");
+})
+
+module.exports = client;
--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 const Koa = require('koa');
 const Router = require('koa-router');
-
 const koa = new Koa();
+
 var router = new Router();
 
 //rota simples pra testar se o servidor está online
@@ -18,8 +18,8 @@ router.get('/', async (ctx) => {
   ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
 });
 
-//Uma rota de exemplo simples aqui.
-//As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
+// //Uma rota de exemplo simples aqui.
+// //As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
 router.get('/users', async (ctx) => {
     ctx.status = 200;
     ctx.body = {total:0, count: 0, rows:[]}
